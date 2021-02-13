5187605e3ce2601b6ab8badf8b38e1ee23aed85d
pr1-21-shilovda <pr1-21-shilovda@K21503N03>
2021-02-13 12:43:05 +0500

    13.02.2021 12:42

---
+++ DBEntities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _11_02_21_prb.DBEntities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Ip { get; set; }
+        public string LastEnter { get; set; }
+        public string Services { get; set; }
+        public int TypeId { get; set; }
+    }
+}
---
+++ StaticInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _11_02_21_prb
+{
+    public static class StaticInfo
+    {
+        public static MainWindow MainWindow;
+        public static string DBConnectionString = "Data Source=SQLDEM\\SQLDEM;Initial Catalog=pr1-21-shilovda2;Integrated Security=True";
+    }
+}
